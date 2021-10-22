18e817b3f2a2d554931b2e4d9754e855649f363c
sbrick26 <swayambarik@gmail.com>
2021-10-21 17:50:02 -0700

    dark mode override comments added

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // force dark/light mode even is user does that (not whole app)
+        // for whole app do it in info.plist
+        overrideUserInterfaceStyle = .dark
+        
+    }
+    
+    // wont work when user comes back from changing setting without quitting app
+    
+    override func viewWillAppear(_ animated: Bool) {
+        changeButt()
+        print("View Will Appear")
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        changeButt()
+        print("trait collection did change")
+    }
+    
+    
+    
+    
+    func changeButt() {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }
+        else {
+            changeButton.tintColor = UIColor.black
+        }
     }
 
 
